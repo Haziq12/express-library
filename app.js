5e7d2d43ddd2379d5a795f8ab27b367bb3707564
Haziq Naeem <haziqnaeem36@gmail.com>
2023-03-03 20:15:25 -0500

    use forEach on methods, set method properties on app object and assign it anonymous function that will call lazyrouter and return this

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
+import methods from 'methods'
+
 const app = {}
 export default app 
 
 app.init = function() {
   this.cache = {}
   this.engines = {}
-  this.settigs = {}
+  this.settings = {}
   this._router = undefined 
 }
+
+let slice = Array.prototype.slice 
+
+methods.forEach((method) => {
+  app[method] = (path) => {
+    this.lazyrouter() 
+    let route = this._router.route(path) 
+    route[method].apply(route, slice.call(arguments, 1))
+    return this; 
+  }
+})
